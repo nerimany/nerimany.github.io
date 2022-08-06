04686b4ca2a9f8bde8cba9b61d29c8396debc979
nerimany <97762104+nerimany@users.noreply.github.com>
2022-08-06 22:01:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2022-08-12 20:00", "Australia/Sydney");
+  let targetDate = moment.tz("2022-08-12 13:30", "Australia/Sydney");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
